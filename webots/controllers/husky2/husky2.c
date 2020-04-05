5d75d06f327a89e443890b1ff0a703a3f06ec1ad
Evan Thomas <evan@evanjt.com>
2020-04-05 12:58:28 +1000

    Moves robot, logs output to csv

--- webots/controllers/husky2/husky2.c
+++ webots/controllers/husky2/husky2.c
@@ -12,16 +12,17 @@
 #include <stdlib.h>
 #include <webots/motor.h>
 #include <webots/robot.h>
-#include <webots/supervisor.h>
+// #include <webots/supervisor.h>
 #include <webots/lidar.h>
+#include <webots/gps.h>
 
 static int time_step = 0;
 static WbDeviceTag velodyne;
-/*
- * You may want to add macros here.
- */
+static WbDeviceTag gnss;
+
 #define TIME_STEP 64
 #define ROBOT_HEIGHT 1.0 // As to not include points beneath
+#define VALUES_IN_GNSS 3
 
 // Calculates the distance between the LiDAR and a point with
 // euclidean distance. LiDAR relative to a point is always (0, 0, 0)
@@ -31,14 +32,39 @@ double euclidean_dist_from_LiDAR(double x, double y, double z) {
 
 int main(int argc, char **argv) {
   // int velodyne_sampling_period = 0;
-  
+
   // Initialise robot
   wb_robot_init();
+  WbDeviceTag front_left_wheel = wb_robot_get_device("front left wheel");
+  WbDeviceTag front_right_wheel = wb_robot_get_device("front right wheel");
+  WbDeviceTag back_left_wheel = wb_robot_get_device("back left wheel");
+  WbDeviceTag back_right_wheel = wb_robot_get_device("back right wheel");
+
+  // init motors
+  wb_motor_set_position(front_left_wheel, INFINITY);
+  wb_motor_set_position(front_right_wheel, INFINITY);
+  wb_motor_set_position(back_left_wheel, INFINITY);
+  wb_motor_set_position(back_right_wheel, INFINITY);
+
+  // init speed for each wheel
+  double back_left_speed = 0.0, back_right_speed = 0.0;
+  double front_left_speed = 0.0, front_right_speed = 0.0;
+  wb_motor_set_velocity(front_left_wheel, front_left_speed);
+  wb_motor_set_velocity(front_right_wheel, front_right_speed);
+  wb_motor_set_velocity(back_left_wheel, back_left_speed);
+  wb_motor_set_velocity(back_right_wheel, back_right_speed);
   
+  // init dynamic variables
+  double left_obstacle = 0.0, right_obstacle = 0.0;
+
   // Setup the Velodyne LiDAR
   time_step = wb_robot_get_basic_time_step();
   velodyne = wb_robot_get_device("Velodyne HDL-32E");
+  gnss = wb_robot_get_device("SwiftNav RTK");
+  
+  // Enable sensors
   wb_lidar_enable(velodyne, time_step);
+  wb_gps_enable(gnss, time_step);
   // velodyne_sampling_period = wb_lidar_get_sampling_period(velodyne);
 
   // Velodyne status messages
@@ -67,24 +93,45 @@ int main(int argc, char **argv) {
     // Assign point stream from Velodyne
     const int point_count = wb_lidar_get_number_of_points(velodyne);
     const WbLidarPoint* point_stream = wb_lidar_get_point_cloud(velodyne); 
+    const double* gnss_stream = wb_gps_get_values(gnss);
+    double gnss_location[VALUES_IN_GNSS];
+    
+    // Move robot forwards
+    wb_motor_set_velocity(front_left_wheel, 5);
+    wb_motor_set_velocity(front_right_wheel, 5);
+    wb_motor_set_velocity(back_left_wheel, 5);
+    wb_motor_set_velocity(back_right_wheel, 5);
+    
+    // Three values in stream for x, y, z. Unpack in the loop    
+    for (int i = 0; i < VALUES_IN_GNSS; i++) {
+      // printf("%f\n", gnss_stream[i]);
+      gnss_location[i] = *gnss_stream;
+      gnss_stream++;
+    }
+    printf("%f, %f, %f\n", gnss_location[0], 
+                           gnss_location[1], 
+                           gnss_location[2]);    
+                           
     for (int i = 0; i < point_count; i++) {
-      if (point_stream->y > -1.0 
+      if (point_stream->y > -ROBOT_HEIGHT 
          && point_stream->x != 0.0 
          && point_stream->y != 0.0
          && point_stream->z != 0.0) {
-        fprintf(fp, "%.2f\t%.2f\t%.2f\t%.2f\t%d\t%.2f\n", point_stream->time, 
-                point_stream->x, point_stream->y, point_stream->z,
-                point_stream->layer_id, 
-                euclidean_dist_from_LiDAR(point_stream->x,
-                point_stream->y, point_stream->z));
+         // double x = point_stream->x + gnss_location[0];
+         // double y = point_stream->y + gnss_location[1];
+         // double z = point_stream->z + gnss_location[2];         
+         double x = point_stream->x;
+         double y = point_stream->y;
+         double z = point_stream->z;
+        fprintf(fp, "%.2f\t%.2f\t%.2f\t%.2f\t%d\t%.2f\n", 
+                x, y, z, point_stream->time, point_stream->layer_id, 
+                euclidean_dist_from_LiDAR(x, y, z));
       }
       point_stream++;  // Increment the stream
     }
-    
-    
-    
    };
    
    fclose(fp);
+   wb_robot_cleanup();
   return 0;
 }
