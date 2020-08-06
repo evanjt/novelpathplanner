4d9952dfab52f9fb9913aae6a8930d242c0080ca
Evan Thomas <evan@evanjt.com>
2020-08-06 23:11:45 +1000

    Output 3d array of lidar points, split into 2 in cluster_points

--- webots/controllers/pioneer3at/pioneer3at.py
+++ webots/controllers/pioneer3at/pioneer3at.py
@@ -9,21 +9,22 @@ import numpy as np
 from sklearn.cluster import DBSCAN
 from sklearn import linear_model
 
-def cluster_points(array, ransac_threshold=0.98):
-
+def cluster_points(3darray, ransac_threshold=0.98):
+    # Create 2d array from the x and z columns (first and last)
+    array = 3darray[:, [0,2]]
     clustering = DBSCAN(eps=0.25, min_samples=10).fit(array)
     count_high_ransac = 0
     xy_of_inliers = []
-    
+
     for cluster in np.unique(clustering.labels_):
-    
+
         if cluster >= 0:
             # Split array into two tables x and y
             subX = array[clustering.labels_ == cluster]
             x, y = subX.reshape(-1,2).T
             x = x.reshape(-1,1)
             y = y.reshape(-1,1)
-            
+
             # Fit a line quickly with RANSAC over the 2D data
             # Setup RANSAC with min_samples on total number of points
             ransac = linear_model.RANSACRegressor(min_samples=x.shape[0])
@@ -34,38 +35,43 @@ def cluster_points(array, ransac_threshold=0.98):
             # Determine fit of line with the same points (not ideal, would be
             # better to split 80/20% dataset and test data with independent data
             ransac_score = ransac.score(x,y)
-            
+
             if ransac_score > ransac_threshold:
                 count_high_ransac += 1
                 # print(inlier_mask)
                 # print("Cluster #: {} RANSAC Score: {:.2f}".format(cluster, ransac_score))
                 # print(np.hstack((x[inlier_mask], y[inlier_mask])).tolist())
-                
+
                 # Combine x and y together of the inliers of the cluster, form a list, append
                 xy_of_inliers.append(np.hstack((x[inlier_mask], y[inlier_mask])).tolist())
                 # print(np.hstack((x[inlier_mask], y[inlier_mask])).shape, x.shape, y.shape)
-                
+
     print("Suitable RANSAC clusters: {}".format(count_high_ransac))
-    
+
     return xy_of_inliers
-        
-def capture_lidar_scene(lidar_device, path='\\Users\\joshc\Documents\\MCENG\\2020\ENGR90038\\simulation\\output\\points2.csv'):
+
+def capture_lidar_scene(lidar_device, path='points_evan.csv'):
 
     point_list = []
-    
+
     with open(path, 'w', newline='') as outfile:
         csvwriter = csv.writer(outfile)
-        
-        for i in range(4):      
+
+        for i in range(10):
             robot.step(timestep)
-            
-            for row in lidar_device.getPointCloud():          
-                dist = math.sqrt(row.x**2 + row.y**2 + row.z**2)
-            
-                if row.y > -0.06 and dist < 8:
-                    csvwriter.writerow([row.x, row.y, row.z])
-                    point_list.append((row.x, row.z))
-            
+
+            for row in lidar_device.getPointCloud():
+                # dist = math.sqrt(row.x**2 + row.y**2 + row.z**2)
+                # lidar_dist_threshold = 4
+                # if row.y > -0.6 and \
+                   # row.x == 0 and \
+                   # row.y == 0 and \
+                   # row.z == 0 and \
+                   # row.x < lidar_dist_threshold and row.x > -lidar_dist_threshold and \
+                   # row.z < lidar_dist_threshold and row.z > -lidar_dist_threshold:
+                    csvwriter.writerow([row.x, row.y, row.z, row.layer_id])
+                    point_list.append((row.x, row.y, row.z))
+
     return np.array(point_list)
 
 def detect_obstacle(robot, hokuyo, width, halfWidth, rangeThreshold, maxRange,  braitenbergCoefficients):
@@ -73,36 +79,36 @@ def detect_obstacle(robot, hokuyo, width, halfWidth, rangeThreshold, maxRange,  
     # set obstacle counters
     leftObstacle = 0.0
     rightObstacle = 0.0
-    
+
     values = hokuyo.getRangeImage()
-    
+
     for k in range(math.floor(halfWidth)):
         if values[k] < rangeThreshold:
             leftObstacle += braitenbergCoefficients[k] * (1.0 - values[k] / maxRange)
-            
+
         j = width - k - 1;
-        
+
         if values[j] < rangeThreshold:
             rightObstacle += braitenbergCoefficients[k] * (1.0 - values[j] / maxRange)
-            
+
     return leftObstacle, rightObstacle, leftObstacle + rightObstacle
 
 def prepare_to_map(robot, timestep, imu, targetBearing):
 
     # Loop for rotating the robot side on with the feature
     while robot.step(timestep) != -1:
-    
+
         # Continually calculate and update robot bearing
         currentBearing = robot_bearing(imu)
-        
+
         # Move the robot until side on with the target feature
         # Once side on mark the startingposition of the feature mapping
         if abs(targetBearing - currentBearing) > 1 and (targetBearing - currentBearing + 360) % 360 > 180:
             set_velocity(wheels, -MAX_SPEED, MAX_SPEED)
-            
+
         elif abs(targetBearing - currentBearing) > 1 and (targetBearing - currentBearing + 360) % 360 < 180:
             set_velocity(wheels, MAX_SPEED, -MAX_SPEED)
-        
+
         else:
             return
 
@@ -110,7 +116,7 @@ def feature_mapping(robot, timestep, wheels, gps, hokuyo, width, threshold):
 
     # alter threshold for sonar offset
     threshold = threshold
-    
+
     # Calculate starting position
     robot.step(timestep)
     currentPos = robot_position(gps)
@@ -122,40 +128,40 @@ def feature_mapping(robot, timestep, wheels, gps, hokuyo, width, threshold):
 
     # Loop for feature mapping
     while robot.step(timestep) != -1:
-        
+
         # Continually calculate and update robot position and distance to feature mapping start point
         currentPos = robot_position(gps)
         distanceToStart = target_distance(currentPos, startingPos)
         values = hokuyo.getRangeImage()
-        
+
         # Navigate the robot around the feature until it returns to its starting point
         if hasMoved and distanceToStart < 1:
             return
-                
+
         elif not hasMoved and distanceToStart > 1:
             hasMoved = True
-            
+
         else:
             if values[side] < threshold or values[frontSide] < threshold+0.9:
                 set_velocity(wheels, MAX_SPEED, MAX_SPEED*0.6)
-        
+
             elif values[side] > threshold+0.1 or values[frontSide] > threshold+1:
                 set_velocity(wheels, MAX_SPEED*0.6, MAX_SPEED)
-        
+
             else:
                 set_velocity(wheels, MAX_SPEED, MAX_SPEED)
-    
+
 def getBraitenberg(robot, width, halfWidth):
 
     braitenbergCoefficients = []
-    
+
     for i in range(math.floor(width)):
         braitenbergCoefficients.append(gaussian(i, halfWidth, width / 5))
-        
+
     return braitenbergCoefficients
 
 def gaussian(x, mu, sigma):
-    
+
     return (1.0 / (sigma * math.sqrt(2.0 * math.pi))) * math.exp(-((x - mu) * (x - mu)) / (2 * sigma * sigma))
 
 def set_velocity(wheels, leftSpeed, rightSpeed):
@@ -169,9 +175,9 @@ def robot_position(gps):
 
     currentGPSPos = gps.getValues()
     currentPos = location_offset(currentGPSPos, 0, -0.3, -0.2)
-    
+
     return currentPos
-      
+
 def robot_bearing(imu):
 
     return (math.degrees(imu.getRollPitchYaw()[2]) *-1 + 360) % 360
@@ -180,14 +186,14 @@ def target_bearing(current, target):
 
     displacement = difference(current, target)
     bearingToTarget = bearing(displacement)
-    
+
     return bearingToTarget
 
 def target_distance(current, target):
 
     displacement = difference(current, target)
     distanceToTarget = distance(displacement)
-    
+
     return distanceToTarget
 
 def location_offset(position, x, y, z):
@@ -217,7 +223,7 @@ def elevation(displacement, dist):
 
 # define home location
 HOME_LOCATION  =(0, 0, 0)
-TARGET_POSITIONS = [location_offset(HOME_LOCATION, 0, 0, 7), 
+TARGET_POSITIONS = [location_offset(HOME_LOCATION, 0, 0, 7),
                     location_offset(HOME_LOCATION, 0, 0, -7),
                     location_offset(HOME_LOCATION, 7, 0, 0),
                     location_offset(HOME_LOCATION, -7, 0, 0),
@@ -267,7 +273,7 @@ imu.enable(timestep)
 # cameraLeft.enable(timestep)
 # cameraRight = robot.getCamera("MultiSenseS21 right camera")
 # cameraRight.enable(timestep)
-    
+
 wheels = []
 wheelNames = ['front left wheel', 'front right wheel','back left wheel', 'back right wheel']
 for i in range(4):
@@ -297,10 +303,10 @@ for i in range(len(TARGET_POSITIONS)):
     currentPos = robot_position(gps)
     targetBearing = target_bearing(currentPos, TARGET_POSITIONS[i]) # need to reset bearing to feature every few meters to account for error in initial course
     flag = False
-    
+
     # Navigate robot to the feature
     while robot.step(timestep) != -1:
-    
+
         # Continually calculate and update robot position, bearing and distance to target feature
         currentPos = robot_position(gps)
         currentBearing = robot_bearing(imu)
@@ -308,34 +314,34 @@ for i in range(len(TARGET_POSITIONS)):
 
         # Continually detect obstacles
         obstacle = detect_obstacle(robot, hokuyoFront, hkfWidth, hkfHalfWidth, hkfRangeThreshold, hkfMaxRange,  hkfBraitenbergCoefficients)
-        
+
         # Once within range map the feature stop once returned home
         if targetDistance > 3 and obstacle[2] > OBSTACLE_THRESHOLD:
             speed_factor = (1.0 - DECREASE_FACTOR * obstacle[2]) * MAX_SPEED / obstacle[2]
             set_velocity(wheels, speed_factor * obstacle[0], speed_factor * obstacle[1])
-            
+
         elif targetDistance > 3 and obstacle[2] > OBSTACLE_THRESHOLD-0.05:
             set_velocity(wheels, MAX_SPEED, MAX_SPEED)
             flag = False
-        
+
         elif flag == False:
             targetBearing = target_bearing(currentPos, TARGET_POSITIONS[i])
             flag = True
-        
+
         elif targetDistance > 3 and abs(targetBearing - currentBearing) > 1 and (targetBearing - currentBearing + 360) % 360 > 180:
             set_velocity(wheels, MAX_SPEED*0.5, MAX_SPEED)
-            
+
         elif targetDistance > 3 and abs(targetBearing - currentBearing) > 1 and (targetBearing - currentBearing + 360) % 360 < 180:
             set_velocity(wheels, MAX_SPEED, MAX_SPEED*0.5)
-            
+
         elif targetDistance > 3:
             set_velocity(wheels, MAX_SPEED, MAX_SPEED)
-            
+
         elif i == len(TARGET_POSITIONS)-1:
             print("Survey complete")
             set_velocity(wheels, 0, 0)
-            break  
-                 
+            break
+
         else:
             prepare_to_map(robot, timestep, imu, (currentBearing + 90) % 360)
             feature_mapping(robot, timestep, wheels, gps, hokuyoFront, hkfWidth, 1.5)
